849fb1af80968653a7fc79bd057e5814d76c6dee
Thomas Perl <m@thp.io>
2011-12-10 01:49:56 +0100

    Multi-controller test

--- motion.py
+++ motion.py
@@ -10,161 +10,119 @@ from PySide.QtDeclarative import *
 import sys
 import os
 
-#/** sensor werte seit der letzten lenk operation */
-sensor_values = []
-
-#/**
-# * Damit bekommen wir die sensor updates
-# *
-# */
-def update_sensor(gx, gy, gz):
-    global sensor_values
-    #fill array with new value
-    sensor_values.append((gx,gy,gz))
-
-
-#/**
-# * Hier liefern wir die relative lenkbewegung also die lenkbewegung relativ zur bisherige richtung
-# * @return int
-# */
-def get_rotation():
-	#//calculate the new rotation
-	#wenn im interval die x componente meistens positiv war dann haben wir eine rechtsdrehung
-	#sort sensor values by y component
-        global sensor_values
-	sensor_values.sort()
-
-	#get the median value
-	median = sensor_values[len(sensor_values)/2]
-
-
-	if median > 0:
-		direction = +1
-	else:
-		direction = -1
-
-	#wenn das hole den maximal wert der liste... evt haben wir so die direktion billig gleich dabei
-
-        X, Y, Z = range(3)
-
-	if direction == -1:
-		amplitude = min(sensor_values)[Y]
-	else:
-		amplitude = max(sensor_values)[Y]
-
-        sensor_values = []
-
-	return direction * amplitude
-
-
-
-#/** constant:: if the current ax, ay, az values are smaller than these limits, the position is correct , if the limit is zero, the value is to be ignored */
-correct_sensor_position_limits = (0, 700, 0)
-
-current_sensor_position = (0,0,0)#//(ax,ay,az);
-
-#/**
-# * 	hier setzen wir die current_sensor_position
-# */
-def set_current_sensor_position(ax,ay,az):
-    global current_sensor_position
-    current_sensor_position = (ax,ay,az)
-
-#/**
-# * Richtige position anzeigen
-# * @return boolean
-# */
-def position_is_correct():
-
-	cur_x, cur_y, cur_z = map(abs, current_sensor_position)
-
-	limit_x, limit_y, limit_z = correct_sensor_position_limits
-
-	correct = True
-
-	#//wenn der wert igoriert werden soll(limit = 0) oder wenn er innerhalb des limits ist
-	if limit_x != 0 and cur_x <= limit_x:
-		correct = False
-
-	if limit_y != 0 and cur_y > limit_y:
-		correct = False
-
-	if limit_z != 0 and cur_z <= limit_z:
-		correct = False
-
-
-	return correct
-
-import sys
-
 # XXX: Should not hardcode path to the psmove module here
 sys.path.insert(0, '/home/thp/src/psmoveapi/build/')
 
 import psmove
-move = psmove.PSMove()
 
+class Controller(QObject):
+    # constant:: if the current ax, ay, az values are smaller than these
+    # limits, the position is correct , if the limit is zero, the value is to
+    # be ignored
+    CORRECT_SENSOR_POSITION_LIMITS = (0, 700, 0)
 
-
-while False:
-    if move.poll():
-        #print move.ax, move.ay, move.az
-        update_sensor(move.gx, move.gy, move.gz)
-        set_current_sensor_position(move.ax, move.ay, move.az)
-
-        rotation = get_rotation()
-
-        #print current_sensor_position
-        if position_is_correct():
-            move.set_leds(0, 0, 0)
-
-            if rotation > 400:
-                move.set_leds(0, 255, 0)
-                print 'RIGHT', rotation
-            elif rotation < -400:
-                move.set_leds(0, 0, 255)
-                print 'LEFT', rotation
-        else:
-            move.set_leds(255, 0, 0)
-
-        move.update_leds()
-
-class Steering(QObject):
-    def __init__(self):
+    def __init__(self, id):
         QObject.__init__(self)
+        self.id = id
+        self.move = psmove.PSMove(id)
+        self.sensor_values = []
+        self.current_sensor_position = (0, 0, 0)
+
 
     @Slot(result=int)
     def get_steering(self):
-        global move
         result = 0
-        move.poll()
-        update_sensor(move.gx, move.gy, move.gz)
-        set_current_sensor_position(move.ax, move.ay, move.az)
+        self.move.poll()
+        self.update_sensor(self.move.gx, self.move.gy, self.move.gz)
+        self.set_current_sensor_position(self.move.ax, self.move.ay,
+                self.move.az)
 
-        rotation = get_rotation()
+        rotation = self.get_rotation()
 
         #print current_sensor_position
-        if position_is_correct():
-            move.set_leds(0, 0, 0)
+        if self.position_is_correct():
+            self.move.set_leds(0, 0, 0)
+            self.move.set_rumble(0)
 
             if rotation > 400:
-                move.set_leds(0, 255, 0)
-                print 'RIGHT', rotation
-                result = +1
+                #move.set_leds(0, 255, 0)
+                #print 'RIGHT', rotation
+                result = 1
             elif rotation < -400:
-                move.set_leds(0, 0, 255)
-                print 'LEFT', rotation
+                #move.set_leds(0, 0, 255)
+                #print 'LEFT', rotation
                 result = -1
         else:
-            move.set_leds(255, 0, 0)
+            self.move.set_leds(255, 0, 0)
+            self.move.set_rumble(200)
 
-        move.update_leds()
+        self.move.update_leds()
         return result
+
+
+    def update_sensor(self, gx, gy, gz):
+        self.sensor_values.append((gx,gy,gz))
+
+    def get_rotation(self):
+        #//calculate the new rotation
+        # wenn im interval die x componente meistens positiv war dann haben
+        # wir eine rechtsdrehung
+        self.sensor_values.sort()
+
+        # get the median value
+        median = self.sensor_values[len(self.sensor_values)/2]
+
+        if median > 0:
+            direction = 1
+        else:
+            direction = -1
+
+        # wenn das hole den maximal wert der liste... evt haben wir so die
+        # direktion billig gleich dabei
+        X, Y, Z = range(3)
+
+        if direction == -1:
+            amplitude = min(self.sensor_values)[Y]
+        else:
+            amplitude = max(self.sensor_values)[Y]
+
+        self.sensor_values = []
+        return direction * amplitude
+
+    def set_current_sensor_position(self, ax,ay,az):
+        self.current_sensor_position = (ax,ay,az)
+
+    #/**
+    # * Richtige position anzeigen
+    # * @return boolean
+    # */
+    def position_is_correct(self):
+        cur_x, cur_y, cur_z = map(abs, self.current_sensor_position)
+        limit_x, limit_y, limit_z = self.CORRECT_SENSOR_POSITION_LIMITS
+
+        # wenn der wert igoriert werden soll(limit = 0) oder wenn er innerhalb
+        # des limits ist
+        if limit_x != 0 and cur_x <= limit_x:
+            return False
+
+        if limit_y != 0 and cur_y > limit_y:
+            return False
+
+        if limit_z != 0 and cur_z <= limit_z:
+            return False
+
+        return True
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    steering = Steering()
     view = QDeclarativeView()
+
+    steering = Controller(0)
     view.rootContext().setContextProperty('steering', steering)
+
+    pumping = Controller(1)
+    view.rootContext().setContextProperty('pumping', pumping)
+
     home = os.path.dirname(__file__)
     view.setSource(os.path.join(home, 'qml', 'motion.qml'))
     view.setResizeMode(QDeclarativeView.SizeViewToRootObject)
--- qml/motion.qml
+++ qml/motion.qml
@@ -19,12 +19,15 @@ Rectangle {
         running: true
         interval: 30
         repeat: true
+
         onTriggered: {
-            tank.rotation += 5*steering.get_steering()
+            tank.rotation += 15*steering.get_steering()
+            tank2.rotation += 15*pumping.get_steering()
         }
     }
 
     Tank {
+        id: tank2
         color: 'red'
         x: 600
         y: 400
